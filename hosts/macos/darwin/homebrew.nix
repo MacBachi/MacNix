6696c14e9cf8b324196bc68d7a31257731b87dde
MacBachi <mac@bachi.at>
2025-11-19 16:37:28 +0100

    now really disabled mas apps

--- hosts/macos/darwin/homebrew.nix
+++ hosts/macos/darwin/homebrew.nix
@@ -85,17 +85,14 @@
       "no-more-secrets"         # Recreates the SETEC ASTRONOMY effect from 'Sneakers'
     ];
     masApps = {
-      "1Password for Safari" = 1569813296;
-      "Paperparrot" = 1663665267;
-      "Wireguard" = 1451685025;
-      "Actions for Obsidian" = 1659667937;
-      "uBlock Origin Lite" = 6745342698;
-      "Vinegar" = 1591303229;
-      "1Blocker" = 1365531024;
-      "Goodnotes: KI-Notizen, PDF" = 1444383602;
-      "Microsoft PowerPoint" = 462062816;
-      "Microsoft Excel" = 462058435;
-      "Microsoft Word" = 462054704;
+            #"1Password for Safari" = 1569813296;
+            #"Paperparrot" = 1663665267;
+            #"Wireguard" = 1451685025;
+            #"Actions for Obsidian" = 1659667937;
+            #"uBlock Origin Lite" = 6745342698;
+            #"Vinegar" = 1591303229;
+            #"1Blocker" = 1365531024;
+            #"Goodnotes: KI-Notizen, PDF" = 1444383602;
     };
   };
 }
